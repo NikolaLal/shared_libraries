6866bceae9084d5ce03cdc36cdd6795134014972
NikolaLal <niklalatovic@gmail.com>
2018-02-21 17:31:59 +0100

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -4,11 +4,8 @@ def call(body) {
     body.delegate = config
     body()
 
-	node {
-	    def server = Artifactory.server ('LinuxartsVM')
-    	    def rtMaven = Artifactory.newMavenBuild()
-            def buildInfo
-	    def mvnHome
+    node {
+	      def mvnHome
 	    // Clean workspace before doing anything
 	    deleteDir()
 
@@ -16,25 +13,15 @@ def call(body) {
 	        stage ('Clone') {
 	        	checkout scm
 			mvnHome = tool 'M3'
-		}
-		    
-		stage ('Artifactory configuration') {
-       			 rtMaven.tool = 'M3' // Tool name from Jenkins configuration
-        		 rtMaven.deployer releaseRepo: 'libs-release-local', snapshotRepo: 'libs-snapshot-local', server: server
-        		 rtMaven.resolver releaseRepo: 'libs-release', snapshotRepo: 'libs-snapshot', server: server
-			 buildInfo = Artifactory.newBuildInfo()
-			 buildInfo.env.capture = true
 	        }
-			
 	        stage ('Build') {
 	              // Run the maven build
      			 if (isUnix()) {
          			sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
       					} else {
-         			bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package)
+         			bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package/)
       				}
 	        }
-		    
 	        stage ('Tests') {
 		        parallel 'static': {
 		            sh "echo 'shell scripts to run static tests...'"
@@ -46,11 +33,6 @@ def call(body) {
 		            sh "echo 'shell scripts to run integration tests...'"
 		        }
 	        }
-		    
-		stage ('Publish build info') {
-        	    server.publishBuildInfo buildInfo
-		}
-		    
 	      	stage ('Deploy') {
 	            sh "echo 'deploying to server ${config.serverDomain}...'"
 	      	}
