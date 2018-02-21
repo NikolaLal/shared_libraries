f3f92629418be235cde46b8b925019f606d6d427
NikolaLal <niklalatovic@gmail.com>
2018-02-21 13:46:50 +0100

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -29,9 +29,9 @@ def call(body) {
 	        stage ('Build') {
 	              // Run the maven build
      			 if (isUnix()) {
-         			sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
+         			sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean install"
       					} else {
-         			bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package/)
+         			bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean install/)
       				}
 	        }
 		    
